8fe83b09ed0cbd365bd3cc38dd6caaa9bdd4c3c0
ammaramja <ammaramja@gmail.com>
2017-09-14 09:39:32 +0530

    MVC AND SQL UPDATES

---
+++ Solutions/ValidationsNauthenNauthoNdatabase/ValidationsNauthenNauthoNdatabase/Models/LoginContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ValidationsNauthenNauthoNdatabase.Models
+{
+    public class LoginContext:DbContext
+    {
+        public DbSet<LoginSubmit> ListOfUsers { get; set;}
+    }
+}
